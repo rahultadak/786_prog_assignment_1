f032dc70bb31b90866b575a8822417f18f797953
Rahul Tadakamadla <rahultadak91@gmail.com>
2015-02-13 01:44:50 -0500

    Stable 1

--- 2dconvol.cu
+++ 2dconvol.cu
@@ -3,14 +3,33 @@
 
 #define DEBUG
 
-__global__ void convol2D (float *a, float *h, float *c)
+__global__ void convol2D (float *a, float *h, float *c, int *a_rows, int *a_cols, int *h_rows, int *h_cols)
 {
     int index_x = blockIdx.x * blockDim.x + threadIdx.x;
     int index_y = blockIdx.y * blockDim.y + threadIdx.y;
+    int grid_width = gridDim.x * blockDim.x;
+    int index = index_y * grid_width + index_x;
 
-    int index = index_y * gridDim.x * blockDim.x + index_x;
+    int block_index = blockDim.x * threadIdx.y + threadIdx.x;
+
+    extern __shared__ float h_sh[];
+    if(block_index < h_rows[0]* h_cols[0])
+        h_sh[block_index] = h[block_index];
+
+    __syncthreads();
+    //Try shared memory loading for h
 
     c[index] = index;
+    int sum = 0;
+    for(int i = 0; i<h_rows[0]; i++) 
+    {
+        for(int j = 0; j<h_cols[0];j++)
+        {
+            if((index_y-i)>=0 & (index_y-i)<=(a_rows[0]-1) & (index_x-j)>=0 & (index_x-j)<=(a_cols[0]-1))
+                sum = sum + (a[(index_y-i)*a_cols[0]+(index_x-j)] * h_sh[i*h_cols[0] + j]);
+        }
+    }
+    c[index_y*grid_width + index_x] = sum;
 }
 
         
@@ -43,6 +62,11 @@ int main (int argc, char *argv[])
     size_t a_size = 0;
     size_t h_size = 0;
     size_t c_size = 0;
+
+    int *a_rows_d = 0;
+    int *a_cols_d = 0;
+    int *h_rows_d = 0;
+    int *h_cols_d = 0;
 
     dim3 block_size;
     dim3 grid_size;
@@ -128,9 +152,9 @@ int main (int argc, char *argv[])
             fscanf(f,"%f",&a_h[i*a_cols + j]);
     }
 
-    for(i = h_rows-1 ; i>=0 ;i--)
+    for(i = 0 ; i<h_rows;i++)
     {
-       for (j = h_cols-1 ; j>=0 ;j--)
+       for (j = 0; j<h_cols ;j++)
        {
             fscanf(f,"%f",&hinv_h[i*h_cols + j]);
        }
@@ -158,16 +182,24 @@ int main (int argc, char *argv[])
     cudaMalloc((void **)&a_d,a_size);
     cudaMalloc((void **)&hinv_d,h_size);
     cudaMalloc((void **)&c_d,c_size);
+    cudaMalloc((void **)&a_rows_d,sizeof(int));
+    cudaMalloc((void **)&a_cols_d,sizeof(int));
+    cudaMalloc((void **)&h_rows_d,sizeof(int));
+    cudaMalloc((void **)&h_cols_d,sizeof(int));
 
     //Copying input data from the Host to Device
     cudaMemcpy(a_d,a_h,a_size,cudaMemcpyHostToDevice);
     cudaMemcpy(hinv_d,hinv_h,h_size,cudaMemcpyHostToDevice);
-
+    cudaMemcpy(a_rows_d,&a_rows,sizeof(int),cudaMemcpyHostToDevice);
+    cudaMemcpy(a_cols_d,&a_cols,sizeof(int),cudaMemcpyHostToDevice);
+    cudaMemcpy(h_rows_d,&h_rows,sizeof(int),cudaMemcpyHostToDevice);
+    cudaMemcpy(h_cols_d,&h_cols,sizeof(int),cudaMemcpyHostToDevice);
+    
     //Setting Op matrix to all zeros
     cudaMemset(c_d,0,c_size);
 
     //Convolution function
-    convol2D<<<grid_size,block_size>>>(a_d,hinv_d,c_d);
+    convol2D<<<grid_size,block_size,h_size>>>(a_d,hinv_d,c_d,a_rows_d,a_cols_d,h_rows_d,h_cols_d);
 
     //Synchronize to wait for the kernel to complete exectution
     cudaThreadSynchronize();
